aa7211835142a9d65103c9e53c1c1a2a3eb85a18
Melos Hasanaj <melos.hasanaj@gmail.com>
2021-12-08 12:34:53 +0100

    added day 1, part 1 solution

---
+++ Day1/Part1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+string result = File.ReadAllText("measurements-1.txt");
+string[] measurements = result.Split("\n");
+
+int? prevNumber = null;
+var count = 0;
+foreach(var measurement in measurements)
+{
+    var nextNumber = Convert.ToInt32(measurement);
+    if (prevNumber != null && nextNumber > prevNumber)
+    {
+        count++;
+    }
+
+    prevNumber = nextNumber;
+}
+
+Console.WriteLine($"The count of measurements increased: {count}");
