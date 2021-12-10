7ade952bc14c488cfdbc24449ada28ede71561f1
Melos Hasanaj <melos.hasanaj@gmail.com>
2021-12-10 10:58:40 +0100

    added part 2

---
+++ Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+static void Part1()
+{
+    string result = File.ReadAllText("measurements-1.txt");
+    string[] measurements = result.Split("\n");
+
+    int? prevNumber = null;
+    var count = 0;
+    foreach(var measurement in measurements)
+    {
+        var nextNumber = Convert.ToInt32(measurement);
+        if (prevNumber != null && nextNumber > prevNumber)
+        {
+            count++;
+        }
+
+        prevNumber = nextNumber;
+    }
+
+    Console.WriteLine($"Day 1, part 1: {count}");
+}
+
+static void Part2()
+{
+    string result = File.ReadAllText("measurements-2.txt");
+    var measurements = result.Split("\n").ToList();
+
+    int? prevSum = null;
+    var count = 0;
+    for (var index = 0; index < measurements.Count; index++)
+    {
+        var nextSum = measurements.Skip(index).Take(3).Select(m => Convert.ToInt32(m)).Sum();
+        if (prevSum != null && nextSum > prevSum)
+        {
+            count++;
+        }
+
+        prevSum = nextSum;
+    }
+
+    Console.WriteLine($"Day 1, part 2: {count}");
+}
+
+Part1();
+Part2();
