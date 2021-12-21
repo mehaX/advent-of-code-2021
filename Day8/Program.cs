5853f4d062d355d4b922dd9e207024916e82412b
Melos Hasanaj <melos.hasanaj@gmail.com>
2021-12-21 14:36:57 +0100

    day8, part1

---
+++ Day8/Program.cs
@@ -0,0 +1,23 @@
+﻿
+static void Part1()
+{
+    var input = File.ReadLines("input.txt");
+    var outputCount = 0;
+    var filteredLengths = new[] { 2, 4, 3, 7 }; // only for 1, 4, 7, 8
+
+    foreach (var row in input)
+    {
+        var output = row.Split(" | ")[1];
+        foreach (var number in output.Split(" "))
+        {
+            if (filteredLengths.Contains(number.Length))
+            {
+                outputCount++;
+            }
+        }
+    }
+
+    Console.WriteLine($"Part1 result: {outputCount}");
+}
+
+Part1();
