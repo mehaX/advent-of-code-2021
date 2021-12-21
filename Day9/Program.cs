733f9650ff1c2fe9a591d938e77d6f9545432cce
Melos Hasanaj <melos.hasanaj@gmail.com>
2021-12-21 17:25:17 +0100

    day9, part2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,6 +1,33 @@
 ﻿
 static void Part1()
 {
+    var heightMap = GetHeightMap();
+    var lowestPoints = GetLowestPoints(heightMap);
+
+    var result = lowestPoints.Select(pt => heightMap[pt.Item1, pt.Item2] + 1).Sum();
+
+    Console.WriteLine($"Part1: {result}");
+}
+
+static void Part2()
+{
+    var heightMap = GetHeightMap();
+    var lowestPoints = GetLowestPoints(heightMap);
+    List<List<(int, int)>> basins = new();
+    
+    foreach (var lowestPoint in lowestPoints)
+    {
+        var basin = new List<(int, int)>();
+        GetBasinFromLowestPoint(heightMap, lowestPoint.Item1, lowestPoint.Item2, ref basin);
+        basins.Add(basin);
+    }
+
+    var result = basins.Select(b => b.Count).OrderByDescending(b => b).Take(3).Aggregate((a, b) => a * b);
+    Console.WriteLine($"Part2: {result}");
+}
+
+static int[,] GetHeightMap()
+{
     var input = File.ReadLines("input.txt").ToArray();
     var heightMap = new int[input.Length, input.First().Length];
 
@@ -12,6 +39,11 @@ static void Part1()
         }
     }
 
+    return heightMap;
+}
+
+static List<(int, int)> GetLowestPoints(int[,] heightMap)
+{
     var lowestPoints = new List<(int, int)>();
     for (var row = 0; row < heightMap.GetLength(0); row++)
     {
@@ -25,9 +57,7 @@ static void Part1()
         }
     }
 
-    var result = lowestPoints.Select(pt => heightMap[pt.Item1, pt.Item2] + 1).Sum();
-
-    Console.WriteLine($"Part1: {result}");
+    return lowestPoints;
 }
 
 static (int, int) LowestPointFromPosition(int[,] heightMap, int row, int col, List<(int,int)> track)
@@ -89,4 +119,34 @@ static (int, int) LowestPointFromPosition(int[,] heightMap, int row, int col, Li
     return lowestPoint;
 }
 
+static void GetBasinFromLowestPoint(int[,] heightMap, int row, int col, ref List<(int, int)> track)
+{
+    if (heightMap[row, col] == 9 || track.Any(t => t.Item1 == row && t.Item2 == col))
+    {
+        return;
+    }
+
+    track.Add((row, col));
+    
+    if (row > 0 && heightMap[row - 1, col] < 9)
+    {
+        GetBasinFromLowestPoint(heightMap, row - 1, col, ref track);
+    }
+
+    if (row < heightMap.GetLength(0) - 1 && heightMap[row + 1, col] < 9)
+    {
+        GetBasinFromLowestPoint(heightMap, row + 1, col, ref track);
+    }
+    if (col > 0 && heightMap[row, col - 1] < 9)
+    {
+        GetBasinFromLowestPoint(heightMap, row, col - 1, ref track);
+    }
+
+    if (col < heightMap.GetLength(1) - 1 && heightMap[row, col + 1] < 9)
+    {
+        GetBasinFromLowestPoint(heightMap, row, col + 1, ref track);
+    }
+}
+
 Part1();
+Part2();
