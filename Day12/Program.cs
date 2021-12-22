05383241afdbc3d7393b23ed7f05d043166ade5d
Melos Hasanaj <melos.hasanaj@gmail.com>
2021-12-22 16:48:34 +0100

    day12, part2, refactoring

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -2,17 +2,17 @@
 using Edges = System.Collections.Generic.List<(string, string)>;
 using Nodes = System.Collections.Generic.List<string>;
 
-static void Part1()
+static void Part(int smallCavesCount = 1)
 {
     var edges = GetEdges();
     var startNode = "start";
-    var solutions = new List<Edges>();
-    var track = new Edges();
+    var solutions = new List<Nodes>();
+    var track = new Nodes(){startNode};
     
-    FindPath(edges, startNode, ref track, ref solutions);
+    FindPath(edges, startNode, ref track, ref solutions, smallCavesCount);
     var result = solutions.Count;
 
-    Console.WriteLine($"Part1: {result}");
+    Console.WriteLine($"With Small caves count of {smallCavesCount}: {result}");
 }
 
 static Edges GetEdges()
@@ -29,37 +29,39 @@ static Edges GetEdges()
     return edges;
 }
 
-static void FindPath(Edges edges, string sourceNode, ref Edges track, ref List<Edges> solutions)
+static void FindPath(Edges edges, string sourceNode, ref Nodes track, ref List<Nodes> solutions, int smallCavesCount)
 {
-    var targetNodes = GetConnectedNodes(edges, sourceNode, track);
+    var targetNodes = GetConnectedNodes(edges, sourceNode, track, smallCavesCount);
     
     foreach (var targetNode in targetNodes)
     {
-        var newList = track.ToList(); // clone
-        newList.Add((sourceNode, targetNode));
+        var newTrack = track.ToList(); // clone
+        newTrack.Add(targetNode);
         if (targetNode.IsEnd())
         {
-            solutions.Add(newList);
+            solutions.Add(newTrack);
         }
         else
         {
-            FindPath(edges, targetNode, ref newList, ref solutions);
+            FindPath(edges, targetNode, ref newTrack, ref solutions, smallCavesCount);
         }
     }
 }
 
-static Nodes GetConnectedNodes(Edges edges, string node, Edges track)
+static Nodes GetConnectedNodes(Edges edges, string node, Nodes track, int smallCavesCount)
 {
     var connectedNodes = edges
         .Where(e => e.HasConnection(node))
         .Select(e => e.TargetNode(node))
-        .Where(n => !n.IsSmallCave() || !track.IsInTrack(n))
+        .Where(n => !n.IsStart())
+        .Where(n => n.IsBigCave() || track.RepeatedSmallCavesCount() <= smallCavesCount)
         .ToList();
 
     return connectedNodes;
 }
 
-Part1();
+Part(1);
+Part(2);
 
 
 public static class Extensions
@@ -71,7 +73,7 @@ public static class Extensions
     
     public static bool IsSmallCave(this string cave)
     {
-        return cave[0] >= 'a' && cave[0] <= 'z';
+        return !cave.IsStart() && !cave.IsEnd() && cave[0] >= 'a' && cave[0] <= 'z';
     }
 
     public static bool IsStart(this string cave)
@@ -94,8 +96,13 @@ public static class Extensions
         return edge.Item1 == sourceNode ? edge.Item2 : edge.Item1;
     }
 
-    public static bool IsInTrack(this Edges track, string node)
+    public static bool IsInTrack(this Nodes track, string node)
     {
-        return track.Any(edge => edge.HasConnection(node));
+        return track.Any(n => n == node);
+    }
+
+    public static int RepeatedSmallCavesCount(this Nodes track)
+    {
+        return track.Where(node => node.IsSmallCave()).Count(smallCave => track.Count(node => node == smallCave) > 1);
     }
 }
