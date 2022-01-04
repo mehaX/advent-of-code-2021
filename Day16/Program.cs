60de67630136940327c549cb10d307a0ca6fbbb2
Melos Hasanaj <melos.hasanaj@gmail.com>
2022-01-04 12:10:39 +0100

    day16, removed Part1()

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,35 +1,30 @@
 ﻿using Day16;
 
-static void Part1()
+var inputHex = File.ReadAllText("input.txt");
+var inputBin = string.Join("", inputHex.ToCharArray().Select(c =>
 {
-    var inputHex = File.ReadAllText("input.txt");
-    var inputBin = string.Join("", inputHex.ToCharArray().Select(c =>
+    var number = Convert.ToInt32(c.ToString(), 16);
+    var result = Convert.ToString(number, 2);
+    if (number < 8)
     {
-        var number = Convert.ToInt32(c.ToString(), 16);
-        var result = Convert.ToString(number, 2);
-        if (number < 8)
-        {
-            result = "0" + result;
-        }
+        result = "0" + result;
+    }
 
-        if (number < 4)
-        {
-            result = "0" + result;
-        }
+    if (number < 4)
+    {
+        result = "0" + result;
+    }
 
-        if (number < 2)
-        {
-            result = "0" + result;
-        }
-        return result;
-    }));
+    if (number < 2)
+    {
+        result = "0" + result;
+    }
+    return result;
+}));
     
 
-    var rootPackage = PackageParser.Parse(ref inputBin);
-    var part1 = rootPackage.CalculateVersionSum();
-    var part2 = rootPackage.CalculateResult();
-    Console.WriteLine($"Part 1: {part1}");
-    Console.WriteLine($"Part 2: {part2}");
-}
-
-Part1();
+var rootPackage = PackageParser.Parse(ref inputBin);
+var part1 = rootPackage.CalculateVersionSum();
+var part2 = rootPackage.CalculateResult();
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
